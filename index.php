bcbe7418c33896c21867cd36fe1690bd95d780f1
Andre Rondi <andre_castro_rondi@hotmail.com>
2021-03-21 04:19:47 -0400

    Integrando PHP com HTML & CSS

--- index.php
+++ index.php
@@ -16,11 +16,13 @@
         <div class="conteudo">
             <div class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 - Básico</h3>
+                    <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
                     </ul>
                 </div>
                 <div class="modulo vermelho">
